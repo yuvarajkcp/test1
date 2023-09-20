4098a5072d0bc54399c4e5e4a36ce2aacad973ab
yuvarajkcp <yuvakcp@gmail.com>
2023-09-20 09:19:37 -0100

    azhagumittai

---
+++ B.java
@@ -0,0 +1,9 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Apm machi epdi iruka");
+		System.out.println("Apm machi epdi iruka");
+		System.out.println("Apm machi epdi iruka");
+	}
+}
